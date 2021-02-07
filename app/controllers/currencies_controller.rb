38e7ed6409b95d9662c9d0d68f33a24ef68ec74b
sgarrick <sgarrick@uchicago.edu>
2021-02-07 19:25:57 +0000

    everything working except rate

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -10,5 +10,32 @@ class CurrenciesController < ApplicationController
     
   end
 
+  def second_currency
+    require "open-uri"
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+    @main_symbol = params.fetch("main_symbol")
+    render({:template => "currency_templates/step_two.html.erb"})
+    
+  end
+
+  def conversion
+    require "open-uri"
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+    @main_symbol = params.fetch("main_symbol")
+    @sec_symbol = params.fetch("second_symbol")
+    @raw_data_rates = open("https://api.exchangerate.host/convert?from=:main_symbol&to=:sec_symbol").read
+    @parsed_data_rates = JSON.parse(@raw_data_rates)
+    @info_hash = @parsed_data_rates.fetch("info")
+    @rate = @info_hash.fetch("rate")
+    render({:template => "currency_templates/conversion.html.erb"})
+    
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 get("/forex", {:controller => "currencies", :action=> "first_currency"})
-
+get("/forex/:main_symbol", {:controller => "currencies", :action=> "second_currency"})
+get("/forex/:main_symbol/:second_symbol", {:controller => "currencies", :action=> "conversion"})
+get("https://api.exchangerate.host/convert?from=:main_symbol&to=:sec_symbol", {:controller => "currencies", :action=> "conversion"})
 end
+
